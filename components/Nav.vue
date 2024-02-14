54ce8a4dcff773f193a8d4f3d4c96749548ff4a6
Christian <christian@mono.studio>
2024-02-14 18:30:51 +0100

    feat: added right side homepage

--- components/Nav.vue
+++ components/Nav.vue
@@ -16,7 +16,7 @@
                         d="M1014.64 969.04L703.71 656.207c57.952-69.408 92.88-158.704 92.88-256.208c0-220.912-179.088-400-400-400s-400 179.088-400 400s179.088 400 400 400c100.368 0 192.048-37.056 262.288-98.144l310.496 312.448c12.496 12.497 32.769 12.497 45.265 0c12.48-12.496 12.48-32.752 0-45.263zM396.59 736.527c-185.856 0-336.528-150.672-336.528-336.528S210.734 63.471 396.59 63.471c185.856 0 336.528 150.672 336.528 336.528S582.446 736.527 396.59 736.527" />
                 </svg>
                 <input
-                    class="w-full h-6 font-bold bg-[#F9F9F9] border border-none  focus:outline-none text-[#171717] placeholder:text-[#a8a8a8] placeholder:font-thin"
+                    class="w-full mx-2 h-6 font-bold bg-[#F9F9F9] border border-none  focus:outline-none text-[#171717] placeholder:text-[#a8a8a8] placeholder:font-thin"
                     placeholder="Search" />
 
             </div>
@@ -24,14 +24,14 @@
         <div class="flex w-1/6 pr-6 gap-x-7">
             <button class="flex items-center gap-x-1 h-14 group">
                 <!-- forse da ricaricare come svg per l'hover effect -->
-                <img src="../public/icons/navWrite.png" alt="write-post" class="h-5 text-[#a8a8a8]group-hover:text-black">
+                <img src="/icons/navWrite.png" alt="write-post" class="h-5 text-[#a8a8a8]group-hover:text-black">
                 <p class="text-[#858585] h-4 mb-1 align-middle font-thin group-hover:text-black">Write</p>
             </button>
             <button class="flex items-center h-14">
-                <img src="../public/icons/navBell.png" alt="Notification">
+                <img src="/icons/navBell.png" alt="Notification">
             </button>
             <button class="flex items-center h-14">
-                <img src="../public/icons/navUser.png" alt="user-photo" class="h-10 rounded-full">
+                <img src="/icons/navUser.png" alt="user-photo" class="h-10 rounded-full">
             </button>
         </div>
     </nav>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,129 @@
 <template>
-    <div>
-        <!--    <h1 class="font-bold text-center bg-yellow-400">
-            index page
-        </h1> -->
+    <div class="px-12 mx-auto">
+        <div class="flex">
+            <!-- Main feed -->
+            <div class="w-3/4 pr-4 overflow-auto">
+                <!-- <div v-for=" post in posts" :key="post.id">
+        </div> -->
+
+            </div>
+
+            <!-- Staff pick -->
+            <div class="sticky top-0 w-1/4 px-2">
+                <!--  <div v-for="pick in staffPicks" :key="pick.id">
+                </div> -->
+                <div class="items-center py-8 text-center">
+                    <button class="w-11/12 p-2 text-white bg-black rounded-2xl">Get unlimited access </button>
+                </div>
+                <h2 class="py-4 font-bold">
+                    Staff pick
+                </h2>
+                <!-- staff post -->
+                <div class="py-2">
+                    <!-- header -->
+                    <div class="flex gap-x-1">
+                        <img src="../public/img/staffUser1.png" alt="Picasso propic" class="h-6 rounded-full" />
+                        <p>Pablo Picasso</p>
+                    </div>
+                    <!-- Title of post -->
+                    <p class="font-bold text-md">
+                        Shattering conventions: why I paint the unseen
+                    </p>
+                </div>
+                <!-- 2nd -->
+                <div class="">
+                    <!-- header -->
+                    <div class="flex gap-x-1">
+                        <img src="../public/img/staffUser2.png" alt="Basquiat propic" class="h-6 rounded-full" />
+                        <p>Jean-Michel Basquiat</p>
+                    </div>
+                    <!-- Title of post -->
+                    <p class="font-bold text-md">
+                        Unmasking power structures, a street art perspective.
+                    </p>
+                </div>
+                <!-- 3rd -->
+                <div class="py-2">
+                    <!-- header -->
+                    <div class="flex gap-x-1">
+                        <img src="../public/img/staffUser3.png" alt="Warhol propic" class="h-6 rounded-full" />
+                        <p>Andy Warhol</p>
+                    </div>
+                    <!-- Title of post -->
+                    <p class="font-bold text-md">
+                        From soup cans to stardom, exploring art in everyday objects
+                    </p>
+                </div>
+                <!-- see full staff page -->
+                <button class="py-4 text-sm text-green-600 ">See the full list</button>
+                <!-- recommended topics -->
+                <h2 class="pt-8 pb-2 font-bold text-md">Recommended topics</h2>
+                <!-- tags -->
+                <div class="flex-wrap -ml-4 space-x-4 space-y-2 ">
+                    <button class="p-2 ml-4 bg-gray-400 rounded-3xl">
+                        Data Science
+                    </button>
+                    <button class="p-2 ml-4 bg-gray-400 rounded-3xl">
+                        Technology
+                    </button>
+                    <button class="p-2 ml-4 bg-gray-400 rounded-3xl">
+                        Self Improvement
+                    </button>
+                    <button class="p-2 ml-4 bg-gray-400 rounded-3xl">
+                        Writing
+                    </button>
+                    <button class="p-2 bg-gray-400 rounded-3xl">
+                        Relationship
+                    </button>
+                </div>
+                <button class="py-4 text-sm text-green-600">See more topics</button>
+                <!-- Follow suggestions -->
+                <h2 class="pt-8 pb-2 font-bold text-md">Who to follow</h2>
+                <!-- profiles -->
+
+                <div class="grid grid-flow-col grid-rows-3 gap-1">
+                    <div class="row-span-3">
+                        <img src="../public/img/suggestedUser.png" alt="monet" class="h-8 mr-4 rounded-full">
+                    </div>
+                    <div class="col-span-2 font-semibold text-md ">Claude Monet</div>
+                    <div class="col-span-2 row-span-2 text-xs font-thin "> Impressionist/Social media manager</div>
+                    <div class="col-span-2 row-span-2">
+                        <button class="bg-white border-black rounded-full border-[1px] w-20 p-1">follow</button>
+                    </div>
+
+                </div>
+                <div class="grid grid-flow-col grid-rows-3 gap-1">
+                    <div class="row-span-3">
+                        <img src="../public/img/suggestedUser1.png" alt="monet" class="h-8 mr-4 rounded-full">
+                    </div>
+                    <div class="col-span-2 font-semibold text-md ">Francisco Goya</div>
+                    <div class="col-span-2 row-span-2 text-xs font-thin "> Romanticism/Crysis manager</div>
+                    <div class="col-span-2 row-span-2">
+                        <button class="bg-white border-black rounded-full border-[1px] w-20 p-1">follow</button>
+                    </div>
+
+                </div>
+                <div class="grid grid-flow-col grid-rows-2 gap-1">
+                    <div class="row-span-2">
+                        <img src="../public/img/suggestedUser2.png" alt="monet"
+                            class="justify-center h-8 mr-4 rounded-full">
+                    </div>
+                    <div class="col-span-2 font-semibold text-md ">Pierre-Auguste Renoir</div>
+                    <div class="col-span-2 row-span-1 text-xs font-thin ">Impressionist/Video maker</div>
+                    <div class="col-span-2 row-span-2">
+                        <button class="bg-white border-black rounded-full border-[1px] w-20 p-1">follow</button>
+                    </div>
+                </div>
+                <button class="py-4 text-sm text-green-600 ">See more suggestions</button>
+                <h2 class="pt-8 pb-2 font-bold text-md">Reading list</h2>
+                <!-- bookmark -->
+                <div class="flex flex-wrap m-0 text-sm">
+                    <span>Click the <img src="/icons/bookmark.png" alt="bookmark icon" class="inline-block h-6">
+                        on any story to easily add it to your reading list or a custom list that you can share</span>
+                </div>
+                <!-- Links -->
+            </div>
+        </div>
     </div>
 </template>
+  
